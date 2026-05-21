8530f7baa8ed67cdb0b75612f33f0f10d7299d2c
Jemadiar1 <jemadiar@hotmail.com>
2026-05-21 09:42:47 -0500

    fix(alembic): shorten revision ID to fit VARCHAR(32)

--- backend/alembic/versions/002_make_channel_mappings_tenant_nullable.py
+++ backend/alembic/versions/002_make_channel_mappings_tenant_nullable.py
@@ -15,7 +15,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "002_make_channel_mappings_tenant_nullable"
+revision: str = "002_channel_nullable"
 down_revision: Union[str, None] = "001"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
